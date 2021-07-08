0b2c2c7506dc2ff5aa02a756c73ef9445b26a176
jeanetteobr <jeanette.obr@gmail.com>
2021-07-08 17:27:35 -0400

    Add favorite toggle to developer component. Add state to app component.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,89 @@
-import React from 'react'
-
-export const Developer = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>Front-End Developer</h2>
-    </div>
-  )
-  // return React.createElement('div', {}, [
-  //     React.createElement('h2', {}, props.name),
-  //     React.createElement('h2', {}, 'Front-End Developer'),
-  // ])
-}
+import React, { useState } from 'react'
 
 export const App = () => {
+  const [developers, setDevelopers] = useState([
+    {
+      name: 'Brian',
+      expertise: ['front-end', 'react'],
+      available_for_hire: true,
+      favorited: false
+    },
+    {
+      name: 'Sara',
+      expertise: ['front-end', 'react', 'graphic design'],
+      available_for_hire: false,
+      favorited: false
+    },
+    {
+      name: 'Greg',
+      expertise: ['front-end', 'react'],
+      available_for_hire: true,
+      favorited: true
+    },
+    {
+      name: 'Logan',
+      expertise: [],
+      available_for_hire: true,
+      favorited: false
+    },
+    {
+      name: 'Shaune',
+      expertise: ['front-end', 'vue'],
+      available_for_hire: true,
+      favorited: false
+    },
+    {
+      name: 'Emily',
+      expertise: ['back-end', 'django'],
+      available_for_hire: false,
+      favorited: true
+    }
+  ])
+
+  const handleFavorite = (faveDevName) => {
+    const newDevelopers = developers.map((dev) => {
+      console.log(faveDevName)
+      if (dev.name === faveDevName && dev.favorited) {
+        return { ...dev, favorited: false }
+      } else {
+        return { ...dev, favorited: true }
+      }
+    })
+    setDevelopers(newDevelopers)
+  }
+
   return (
-    <div>
-      <Developer name='Roan' />
-      <Developer name='Sara' />
-      <Developer name='Brian' />
-      <Developer name='Logan' />
-      <Developer name='Shaune' />
-      <Developer name='Greg' />
+    <main>
+      <h1>Developers for Hire!</h1>
+      {developers.map((dev, idx) => {
+        if (dev.available_for_hire) {
+          return (
+            <Developer favorited={dev.favorited} name={dev.name} expertise={dev.expertise} key={idx} id={idx} handleFavorite={(dev) => handleFavorite(dev)} />
+          )
+        }
+        return (
+          <div key={idx}>
+            <h3 style={{ color: 'red' }}>Not Available</h3>
+            <Developer favorited={dev.favorited} name={dev.name} expertise={dev.expertise} key={idx} id={idx} handleFavorite={(dev) => handleFavorite(dev)} />
+          </div>
+        )
+      })}
+    </main>
+  )
+}
+
+export const Developer = (props) => {
+  const { id, expertise, name, favorited, handleFavorite } = props
+
+  return (
+    <div id={id} className=''>
+      <h1>{name}</h1>
+      {expertise && expertise.map((item, idx) => {
+        return (<p key={idx} id={idx}>{item}</p>)
+      })}
+      <p>{expertise}</p>
+      <button onClick={() => handleFavorite(name)}>{favorited ? '♥️' : '♡'}</button>
+      <h2>{expertise[0]} developer</h2>
     </div>
   )
-
-  //   return React.createElement("div", {}, [
-  //     React.createElement(Developer, { name: "Roan" }),
-  //     React.createElement(Developer, { name: "Sara" }),
-  //     React.createElement(Developer, { name: "Brian" }),
-  //     React.createElement(Developer, { name: "Greg" }),
-  //     React.createElement(Developer, { name: "Shaune" }),
-  //   ]);
 }
